99242bf5b0d634adf229d10bd79fd74827b075f3
abel-baker <seth.keating@gmail.com>
2022-03-23 16:25:35 -0400

    attempted changing button groups per save state--commented out.  basic icon swapping per state

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,14 +34,62 @@ for (let hr = workdayStart; hr < workdayEnd + 1; hr++) {
         <button type="button" class="${buttonClasses}" id="button-save-${hr}th">${saveButtonValue}</button>
     `);
 
-    $(`#button-save-${hr}th`).click(handleSaveButton(hr));
-
     // Display existing data
     if (localStorage.getItem(`${hr}`)) {
       console.log(`Checking for data: ${hr}`);
       $(`#textarea-${hr}th`).val(localStorage.getItem(`${hr}`));
+
+      $(`#button-save-${hr}th > i`).removeClass();
+      $(`#button-save-${hr}th > i`).addClass('bi-bookmark-fill');
+      // setClearButton(hr);
     }
+    
+    $(`#button-save-${hr}th`).click(handleSaveButton(hr));
+    $(`#textarea-${hr}th`).on('input', function() {
+      $(`#button-save-${hr}th > i`).removeClass();
+      $(`#button-save-${hr}th > i`).addClass('bi-bookmark-plus');
+    })
 }
+
+// Changes the default button to a set of save and clear buttons
+// function setClearButton(hr) {
+//   let savedIcon = "bi-bookmark-fill";
+//   let clearIcon = "bi-x";
+
+//   let savedButtonValue = `<i class="${savedIcon}"></i><span class="sr-only">Saved icon</span>`;
+//   let clearButtonValue = `<i class="${clearIcon}"></i><span class="sr-only">Clear icon</span>`;
+
+//   // Remove existing button
+//   $(`#button-save-${hr}th`).remove();
+
+//   let topButtonStyle = "border-top-left-radius: 0 !important";
+//   let bottomButtonStyle = "border-bottom-left-radius: 0 !important";
+
+//   $(`#group-${hr}th`).append(`
+//     <div class="btn-group-vertical flex-grow-0" id="btn-group-${hr}th">
+//       <button type="button" class="btn btn-secondary" id="button-saved-${hr}th" style="${topButtonStyle}">${savedButtonValue}</button>
+//       <button type="button" class="btn btn-secondary" id="button-clear-${hr}th" style="${bottomButtonStyle}">${clearButtonValue}</button>
+//     </div>
+//   `);
+  
+//   $(`#button-saved-${hr}th`).click(handleSaveButton(hr));
+// }
+
+// function setSaveButton(hr) {
+//   let saveIcon = "bi-bookmark";
+//   let saveButtonValue = `<i class="${saveIcon}"></i><span class="sr-only">Save icon</span>`;
+
+//   // Remove existing button group
+//   $(`#button-saved-${hr}th`).remove();
+//   $(`#button-clear-${hr}th`).remove();
+//   $(`#btn-group-${hr}th`).remove();
+
+//   $(`#group-${hr}th`).append(`
+//     <button type="button" class="btn btn-secondary" id="button-save-${hr}th">${saveButtonValue}</button>
+//   `);
+
+//   $(`#button-save-${hr}th`).click(handleSaveButton(hr));
+// }
 
 function handleSaveButton(hr) {
   return function(event) {
@@ -58,5 +106,10 @@ function handleSaveButton(hr) {
 
     console.log(`${hr} Saving: ${$(`#textarea-${hr}th`).val()}`);
     localStorage.setItem(`${hr}`, $(`#textarea-${hr}th`).val() );
+
+    $(`#button-save-${hr}th > i`).removeClass();
+    $(`#button-save-${hr}th > i`).addClass('bi-bookmark-check-fill');
+
+    // setClearButton(hr);
   }
 }
